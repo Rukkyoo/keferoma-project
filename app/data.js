1e85efb88ebe3f52ba4d1ca9e315d0ee82a8ffdf
Michael Omonedo <rukkyomonedo@gmail.com>
2024-11-30 19:26:35 +0100

    Included data.js folder

--- app/data.js
+++ app/data.js
@@ -6,14 +6,14 @@ export const products = [
     name: "0.5 litres Grand Soya Oil",
     description: "100% pure and natural groundnut oil",
     price: "₦9.99",
-    image: "/images/groundnut-oil-1l.jpg",
+    image: "gnut0.5-oil.jpg",
     category: "Oils",
     stock: 50
   },
   {
     id: 2,
     name: "1 litre Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
     image: "/images/organic-groundnut-oil-500ml.jpg",
     category: "Oils",
@@ -22,45 +22,45 @@ export const products = [
   {
     id: 3,
     name: "3 litres Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
-    image: "/images/organic-groundnut-oil-500ml.jpg",
+    image: "gnut3-oil.jpg",
     category: "Oils",
     stock: 30
   },
   {
     id: 4,
     name: "5 litres Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
-    image: "/images/organic-groundnut-oil-500ml.jpg",
+    image: "gnut5-oil.jpg",
     category: "Oils",
     stock: 30
   },
   {
     id: 5,
     name: "10 litres Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
-    image: "/images/organic-groundnut-oil-500ml.jpg",
+    image: "gnut10-oil.jpg",
     category: "Oils",
     stock: 30
   },
   {
     id: 6,
     name: "18 litres Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
-    image: "/images/organic-groundnut-oil-500ml.jpg",
+    image: "gnut18-oil.jpg",
     category: "Oils",
     stock: 30
   },
   {
     id: 7,
     name: "25 litres Grand Soya Oil",
-    description: "Certified organic groundnut oil",
+    description: "Certified organic soya oil",
     price: "₦12.99",
-    image: "/images/organic-groundnut-oil-500ml.jpg",
+    image: "gnut25-oil.jpg",
     category: "Oils",
     stock: 30
   },
